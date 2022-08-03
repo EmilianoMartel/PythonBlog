c3462da7f5c6b941e745e3484d94847c49eae278
emiliano martel <emiliano.d.martel.r@gmail.com>
2022-08-03 20:16:47 -0300

    new

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+    description = models.CharField(max_length=200, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    stock = models.IntegerField()
+    seller = models.CharField(max_length=40)
