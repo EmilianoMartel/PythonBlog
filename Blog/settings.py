b7c62459b32af7bf1b92ae37938a1e81cc114ede
emiliano martel <emiliano.d.martel.r@gmail.com>
2022-08-02 22:38:52 -0300

    base

--- Blog/settings.py
+++ Blog/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
