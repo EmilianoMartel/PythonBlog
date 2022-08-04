a5ab4edb4b8087c6e22541ee36cf2dc4c9db43c8
Anselminie <anselminie@gmail.com>
2022-08-04 14:11:58 -0300

    armado de vista lista de productos y html base y listado

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list_products(request):
+    products = Products.objects.all()
+    context = {
+        'products':products
+    }
+    return render(request, 'products/product_list_cards.html', context=context)
