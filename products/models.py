13d2539ee0212544cf31e6b39b44400357fff4f1
Anselminie <anselminie@gmail.com>
2022-08-04 13:47:15 -0300

    Modifique modelo de productos agregando campos

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    name = models.CharField(max_length=50)
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateTimeField( auto_now=True)
+    description = models.TextField()
+    active =  models.BooleanField(default=True)
+    image_url =  models.URLField()
