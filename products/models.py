a1f2db86dbc66aa0f09a278e74623aaae3a5d368
Anselminie <anselminie@gmail.com>
2022-08-04 18:06:10 -0300

    sumo modelo usuarios y favoritos

--- products/models.py
+++ products/models.py
@@ -1,7 +1,9 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
 class Producto(models.Model):
+    id = models.IntegerField(primary_key=True,auto_created=True)
     name = models.CharField(max_length=50)
     price = models.FloatField()
     created_date = models.DateField(auto_now_add=True)
@@ -10,3 +12,15 @@ class Producto(models.Model):
     active =  models.BooleanField(default=True)
     image_url = models.URLField(default="https://image.shutterstock.com/image-vector/photo-album-picture-collection-line-260nw-256926565.jpg")
     
+
+class User(models.Model): #La idea es agregarle usuarios y que puedan marcar productos como favoritos dentro del listado de productos y en una URL específica puedan ver esa lista
+    id = models.IntegerField(primary_key=True,auto_created=True)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    favs = models.ManyToManyField(Producto) #favoritos
+
+"""    
+class Favorito(models.Model):
+    user_id = IntegerField
+    producto_id = IntegerField
+"""
