dafd2c57ca49bed8c6d2a4a83f8c285a31f51d75
emiliano martel <46003816+Coquito38@users.noreply.github.com>
2022-08-05 18:40:32 -0300

    reseña

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from products.views import list_products, list_content
 urlpatterns = [
     path('list-products/', list_products, name='list_products'),
     path('list-content/', list_content, name='list_content'),
+    path('list-review/', list_content, name='list_review'),
 ]
