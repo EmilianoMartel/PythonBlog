55185ff321444da964573ed6cc33d6f25626619a
emiliano martel <46003816+Coquito38@users.noreply.github.com>
2022-08-05 21:19:23 -0300

    sigue con errores el new

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from products.views import list_products, list_content, new_content, list_review
+from products.views import list_products, list_content, new_content, list_review, search_content
 
 
 urlpatterns = [
     path('list-products/', list_products, name='list_products'),
     path('list-content/', list_content, name='list_content'),
     path('list-review/', list_review, name='list_review'),
-    path('list-content/', list_content, name='list_content'),
-]
+    path('new_content/', new_content, name='new_content'),
+    path('search_content/', search_content, name='search_content'),
+    ]
