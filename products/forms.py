5f37ac40a67cb1f83812fee764aecc117f898cef
emiliano martel <46003816+Coquito38@users.noreply.github.com>
2022-08-07 15:50:28 -0300

    no funciona review

--- products/forms.py
+++ products/forms.py
@@ -1,12 +1,23 @@
 from django import forms
+from products.models import Producto, Contenido, Reseña
 
 class Forms_contenido(forms.Form):
     name = forms.CharField(max_length=50)
-    #categorias = [('P','Película'), ('S', 'Serie'), ('C', 'Corto')] comentados por ahora
-    #category = forms.CharField(max_length=1,choices=categorias) 
+    categorias = [('P','Película'), ('S', 'Serie'), ('C', 'Corto')]
+    category = forms.ChoiceField(choices=categorias) 
     #genero similar a categorias si queremos agregarlo o podría ser una clase aparte para poder gestionarlas via panel de control
     score = forms.FloatField() #este quizás no tenga sentido tenerlo como campo en la base sino más bien que se calcule cada vez que se va a mostrar
-    created_date = forms.DateField()
-    modified_date = forms.DateTimeField()
+    #created_date = forms.DateField()
+    #modified_date = forms.DateTimeField()
     description = forms.CharField()
-    #image_url = forms.URLField(default="https://image.shutterstock.com/image-vector/photo-album-picture-collection-line-260nw-256926565.jpg")
+    image_url = forms.URLField()
+
+class Forms_reseña(forms.Form):
+    name = forms.CharField(max_length=50)
+    description = forms.CharField()
+    #film = forms.ChoiceField(choices = Contenido.objects.name)
+    film = forms.CharField()
+    categorias = [('P','Película'), ('S', 'Serie'), ('C', 'Corto')]
+    category = forms.ChoiceField(choices=categorias) 
+    body = forms.CharField()
+    puntaje = forms.CharField()
